23844237cd49464b6fe9170ec913ac80a5ab3d87
Marcin Butora <marcin.butora@vattenfall.com>
2024-01-02 07:28:34 +0100

    feat: views

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,18 @@ const router = createRouter({
     {
       path: '/about',
       component: () => import("../views/About.vue"),
+    },
+    {
+      path: '/team',
+      component: () => import("../views/Team.vue"),
+    },
+    {
+      path: '/contact',
+      component: () => import("../views/Contact.vue"),
+    },
+    {
+      path: '/settings',
+      component: () => import("../views/Settings.vue"),
     }
   ]
 })
